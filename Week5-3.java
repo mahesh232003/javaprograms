541d4b8df606ff7a3dede555b9db387ba5d5ebbe
mahesh232003 <79948899+mahesh232003@users.noreply.github.com>
2021-12-28 17:50:11 +0530

    Create Week5-3.java

---
+++ Week5-3.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+final class sample
+{
+    final int a=10;
+    int b=10;
+    void display()
+    {
+        System.out.println("We cannot change the final variable");
+        System.out.println("A value is:"+a);
+        b=20;
+        System.out.println("B value after changing:"+b);
+    }
+    final void run()
+    {
+        System.out.println("This is a final method");
+    }
+}
+class sample1
+{
+    void display()
+    {
+        System.out.println("Final method run cannot be overrided");
+        System.out.println("We also cannot inherit methods of a final base class");
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    sample s = new sample();
+	    s.display();
+	    sample1 s1= new sample1();
+	    s1.display();
+	}
+}
