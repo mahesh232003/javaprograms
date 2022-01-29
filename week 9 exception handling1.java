bd9ce7c9f0d4c837df93c21b41723eda04a3f749
mahesh232003 <79948899+mahesh232003@users.noreply.github.com>
2022-01-29 10:10:27 +0530

    Create week 9 exception handling1.java
    try catch block

---
+++ week 9 exception handling1.java
@@ -0,0 +1,22 @@
+class TryCatchExample2 
+{  
+public static void main(String[] args) 
+{  
+try
+{  
+int data=50/0; //may throw exception   
+}  
+//handling the exception  
+catch(ArithmeticException e)  
+{  
+System.out.println(e);  
+}  
+System.out.println("rest of the code");  
+}  
+} 
+
+
+/*output
+java TryCatchExample2
+java.lang.ArithmeticException: / by zero
+rest of the code*/
